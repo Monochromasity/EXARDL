7007e7fee185dff7ae97159c7aeeb4e916165d9a
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-07-26 20:04:16 +0100

    create js to generate list

---
+++ script.js
@@ -0,0 +1,31 @@
+async function fetchlist() {
+  try {
+    const response = await fetch("https://raw.githubusercontent.com/Monochromasity/1.0-demonlist/main/list.json");
+    const list = await response.json();
+    return list;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function printlist() {
+  const list = await fetchlist();
+  const htmllist = document.getElementById("list");
+  for (i = 0; i < list.length; i++) {
+    // Level div
+    const item = document.createElement("div");
+    item.className = "item";
+    // Level rank
+    const rank = document.createElement("p");
+    const ranktxt = document.createTextNode("#".concat(i+1));
+    rank.appendChild(ranktxt);
+    rank.className = "rank";
+    level.appendChild(rank);
+    // Level name
+    const name = document.createElement("p");
+    const nametxt = document.createTextNode(list[i]["level"]);
+    name.appendChild(nametxt);
+    name.className = "name";
+    level.appendChild(name);
+    htmllist.appendChild(level);
+  }
