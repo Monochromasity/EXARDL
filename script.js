a0a194a11f74ef228d4b3b103eba5678c122bf52
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-07-26 20:13:01 +0100

    fix link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchlist() {
   try {
-    const response = await fetch("https://raw.githubusercontent.com/Monochromasity/EXARDL/main/list.json");
+    const response = await fetch("https://raw.githubusercontent.com/Monochromasity/EXARDL/refs/heads/main/list.json");
     const list = await response.json();
     return list;
   } catch (error) {
