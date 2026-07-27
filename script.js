0f4cb9c25e3c3e23995c11b55ae2e8b08b4f5895
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-07-27 17:35:26 +0100

    add selected check with param check

--- script.js
+++ script.js
@@ -14,7 +14,16 @@ async function printlist() {
   for (i = 0; i < list.length; i++) {
     // Level div
     const item = document.createElement("div");
-    item.className = "item";
+    if (window.location.search != "" && window.location.search != "?") {
+      if (i = window.location.search.substring(1)) {
+        item.className = "item selected";
+      } else {
+        item.className = "item";
+      }
+    } else if (i = 0) {
+      window.location.search = "?0";
+      item.className = "item selected";
+    }
     // Level rank
     const rank = document.createElement("div");
     const ranktxt = document.createTextNode("#".concat(i+1));
